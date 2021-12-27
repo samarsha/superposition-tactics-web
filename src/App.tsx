75d1d2f1193f0b19e0abcab8ab2ed032c508c8de
Sarah Marshall <sarah@sarahmarshall.name>
2021-12-26 18:59:06 -0800

    Add command panel editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { CommandPanel } from './CommandPanel';
 
@@ -9,12 +10,20 @@ function App() {
     { attacker: 4, target: 1 }
   ];
 
-  const commands = [{ attacker: 1, target: 4 }];
+  const [commands, setCommands] = useState([{ attacker: 1, target: 4 }]);
   const postamble = [{ attacker: 1, target: 4 }];
 
   return (
     <div className="App">
-      <CommandPanel roster={roster} preamble={preamble} commands={commands} postamble={postamble} />
+      <CommandPanel
+        roster={roster}
+        preamble={preamble}
+        commands={commands}
+        postamble={postamble}
+        onAdd={() => setCommands([...commands, { attacker: 1, target: 4 }])}
+        onChange={(command, index) => setCommands(commands.map((c, i) => i === index ? command : c))}
+        onRemove={index => setCommands(commands.filter((_, i) => i !== index))}
+      />
     </div>
   );
 }
